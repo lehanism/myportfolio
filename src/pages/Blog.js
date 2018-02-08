9eeef472c7cd99c90250428723d5fd5bbba205c5
lehanism <lukelehane@live.com>
2018-02-08 20:02:28 +0000

    added blog panel css

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,20 +3,20 @@ import React from "react";
 import Link from 'gatsby-link';
 
 const PostLink = props =>
-    <div key={props.post.id}>
-        <h1>
-            <Link to={props.post.frontmatter.path}>{props.post.frontmatter.title}</Link>
-        </h1>
-        <h2>{props.post.frontmatter.date}</h2>
-        <p>{props.post.excerpt}</p>
-    </div>
+    <Link to={props.post.frontmatter.path}>
+      <div key={props.post.id} className='blog-panel'>
+          <h1 className='blog-h'>{props.post.frontmatter.title}</h1>
+          <h2 className='blog-date'>{props.post.frontmatter.date}</h2>
+          <p className='blog-ex'>{props.post.excerpt}</p>
+      </div>
+    </Link>
 
 const IndexPage = ({ data: { allMarkdownRemark: { edges } } }) => {
     const Posts = edges
         .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
         .map(edge => <PostLink key={edge.node.id} post={edge.node} />);
 
-    return <div className='page'>{Posts}</div>;
+    return <div className='page'><h1 className='moremargin'><span className='red'>Blog</span> (Scribblings)</h1><div>{Posts}</div></div>;
 };
 
 export default IndexPage;
