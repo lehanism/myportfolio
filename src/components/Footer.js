ababfc78583ab651442727b7c931267ae1bb771f
lehanism <lukelehane@live.com>
2018-02-08 20:21:48 +0000

    changes to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
       <footer>
-        pets pets pets
+        made with <span className='red'>Gatsby</span> by <span className='red'>Luke Lehane</span>
       </footer>
     );
   }
